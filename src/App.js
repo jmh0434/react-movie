4861f93218be0d9065e19452107c28ca6ab2421f
jmh0434 <80581335+jmh0434@users.noreply.github.com>
2022-07-31 17:22:43 +0900

    CSS 모듈

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-
+import Button from "./Button";
+import styles from "./App.module.css" // app 모듈 css
 function App() {
   return (
-    <div></div>
+    <div>
+      <h1 className={styles.title}>Hello, React!</h1> 
+      <Button text = {"Click me"}></Button>
+    </div>
   );
 }
 
