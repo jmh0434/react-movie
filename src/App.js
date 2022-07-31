760db0eb15badae51a95379d484cf2b39bac5b80
jmh0434 <80581335+jmh0434@users.noreply.github.com>
2022-07-31 21:05:49 +0900

    useEffect 연습

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import Button from "./Button";
-import styles from "./App.module.css" // app 모듈 css
+import { useState, useEffect } from "react";
+
 function App() {
-  return (
-    <div>
-      <h1 className={styles.title}>Hello, React!</h1> 
-      <Button text = {"Click me"}></Button>
-    </div>
+ const [counter, setValue] = useState(0);
+   const [keyword, setKeyword] = useState("");
+   const onClick = () => setValue((prev) => prev + 1);
+   console.log("i run all the time");
+   const onChange = (event) => setKeyword(event.target.value);
+   useEffect(() => { // 처음 시작했을 때 실행되는 useEffect
+     console.log("CALL THE API....");
+     console.log("I run only once.");
+   }, []);
+   useEffect(() => { // 처음 시작했을 떄와 keyword 값의 변화가 생기면 실행되는 useEffect
+     console.log("I run when 'keyword' changes.");
+   }, [keyword]);
+   useEffect(() => { // 처음 시작했을 떄와 counter 값의 변화가 생기면 실행되는 useEffect
+     console.log("I run when 'counter' changes.");
+   }, [counter]);
+   useEffect(() => { // 처음 시작했을 때와 keyword 혹은 counter 값의 변화가 생기면 실행되는 useEffect
+     console.log("I run when keyword & counter change");
+   }, [keyword, counter]);
+   return (
+     <div>
+       <input
+         value={keyword}
+         onChange={onChange}
+         type="text"
+         placeholder="Search here..."
+       />
+       <h1>{counter}</h1>
+       <button onClick={onClick}>click me</button>
+     </div>
   );
 }
 
