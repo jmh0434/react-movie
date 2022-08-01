f061bbfd59de0d9fb8c1cbbe1353c1e984efd902
jmh0434 <80581335+jmh0434@users.noreply.github.com>
2022-08-01 14:18:05 +0900

    todo 프로그램

--- src/App.js
+++ src/App.js
@@ -1,35 +1,39 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
+
 
 function App() {
- const [counter, setValue] = useState(0);
-   const [keyword, setKeyword] = useState("");
-   const onClick = () => setValue((prev) => prev + 1);
-   console.log("i run all the time");
-   const onChange = (event) => setKeyword(event.target.value);
-   useEffect(() => { // 처음 시작했을 때 실행되는 useEffect
-     console.log("CALL THE API....");
-     console.log("I run only once.");
-   }, []);
-   useEffect(() => { // 처음 시작했을 떄와 keyword 값의 변화가 생기면 실행되는 useEffect
-     console.log("I run when 'keyword' changes.");
-   }, [keyword]);
-   useEffect(() => { // 처음 시작했을 떄와 counter 값의 변화가 생기면 실행되는 useEffect
-     console.log("I run when 'counter' changes.");
-   }, [counter]);
-   useEffect(() => { // 처음 시작했을 때와 keyword 혹은 counter 값의 변화가 생기면 실행되는 useEffect
-     console.log("I run when keyword & counter change");
-   }, [keyword, counter]);
-   return (
-     <div>
-       <input
-         value={keyword}
-         onChange={onChange}
-         type="text"
-         placeholder="Search here..."
-       />
-       <h1>{counter}</h1>
-       <button onClick={onClick}>click me</button>
-     </div>
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === ''){
+      return; 
+    }
+    else {
+      setToDos((currentArray)=> [toDo, ...currentArray]);
+      setToDo("");
+    }
+  }
+  
+  return (
+    <div>
+      <h1>Todo List ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your todo"
+        ></input>
+        <button>Click</button>
+      </form>
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+      ))}
+      </ul>
+    </div>
   );
 }
 
