4326a1ceab3d880af21d632962317e9000511acc
jmh0434 <80581335+jmh0434@users.noreply.github.com>
2022-08-01 16:23:16 +0900

    cointracker

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [toDo, setTodo] = useState("");
-  const [toDos, setTodos] = useState([]);
-  const onChange = (event) => setTodo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if(toDo === "") {
-      return;
-    }
-    setTodos((currentArray) => [toDo, ...currentArray]); // 배열에 toDo를 넣어라
-    setTodo("");
-  }
+  const [loading, setLoading] = useState(true); 
+  const [coins, setCoins] = useState([]);
 
-  
+  useEffect(()=> {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response)=>response.json())
+      .then((json)=>{
+        setCoins(json);
+        setLoading(false); 
+      });
+  }, []);
+
   return (
     <div>
-     <h1>To-Do List({toDos.length})</h1>
-     <form onSubmit={onSubmit}>
-      <input onChange = {onChange} type = "text" value={toDo} placeholder = "Write your to-dos"></input>
-      <button>Submit</button>
-     </form>
-     <ul>
-      {toDos.map((item, index) => ( // toDos 배열에 접근해서 데이터와 인덱스를 얻어오고
-        <li key={index}>{item}</li> // 인덱스가 key값이 된 채로 데이터를 리스트 형식으로 출력
-      ))}
-     </ul>
+      <h1>All the Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select>
+          {coins.map((coin) => (
+            <option>
+              {coin.name} ({coin.symbol}) : $
+              {coin.quotes.UD.price} USD
+            </option>
+          ))}
+        </select>
+      )}
     </div>
   );
 }
