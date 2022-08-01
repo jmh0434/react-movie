8dce22d6102600c5be644bbefff4087851726137
jmh0434 <80581335+jmh0434@users.noreply.github.com>
2022-08-01 14:44:20 +0900

    todo_self

--- src/App.js
+++ src/App.js
@@ -1,38 +1,31 @@
 import { useState } from "react";
 
-
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
+  const [toDo, setTodo] = useState("");
+  const [toDos, setTodos] = useState([]);
+  const onChange = (event) => setTodo(event.target.value);
   const onSubmit = (event) => {
     event.preventDefault();
-    if (toDo === ''){
-      return; 
+    if(toDo === "") {
+      return;
     }
-    else {
-      setToDos((currentArray)=> [toDo, ...currentArray]);
-      setToDo("");
-    }
+    setTodos((currentArray) => [toDo, ...currentArray]); // 배열에 toDo를 넣어라
+    setTodo("");
   }
+
   
   return (
     <div>
-      <h1>Todo List ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Write your todo"
-        ></input>
-        <button>Click</button>
-      </form>
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
+     <h1>To-Do List({toDos.length})</h1>
+     <form onSubmit={onSubmit}>
+      <input onChange = {onChange} type = "text" value={toDo} placeholder = "Write your to-dos"></input>
+      <button>Submit</button>
+     </form>
+     <ul>
+      {toDos.map((item, index) => ( // toDos 배열에 접근해서 데이터와 인덱스를 얻어오고
+        <li key={index}>{item}</li> // 인덱스가 key값이 된 채로 데이터를 리스트 형식으로 출력
       ))}
-      </ul>
+     </ul>
     </div>
   );
 }
